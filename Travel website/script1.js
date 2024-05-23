5b5ed1e41756bbbe8c4cebb49ff030dc888cb8a3
Poonam-nishad <Poonamn9696@gmail.com>
2024-05-23 14:04:53 +0530

    Add files via upload

---
+++ Travel website/script1.js
@@ -0,0 +1,7 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
